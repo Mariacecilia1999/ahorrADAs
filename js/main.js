82af0116330ded81f3e616fb48f25ae611f14412
Mariacecilia1999 <mariaceciliaconti1999@gmail.com>
2023-09-01 10:59:14 -0300

    Selecting category to edit

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@ const showCategory = (categories) =>{
    $('#showCategory').innerHTML += `<div class= "flex justify-between py-3">
                                        <span class="text-xs bg-emerald-50 text-green-500 px-2 py-1 rounded">${category}</span>
                                        <div>
-                                          <a href="#" class="text-xs text-sky-600 mr-6">Editar</a>
+                                          <a href="#" class="text-xs text-sky-600 mr-6" onclick='editForm(${id})'>Editar</a>
                                           <a href="#" class="text-xs text-sky-600" onclick='deleteCategory(${id})'>Eliminar</a>
                                        </div>
                                     </div>
@@ -75,6 +75,11 @@ const deleteCategory = (id) =>{
    const newCategories = get('categories').filter(category => category.id !== id)
    set('categories', newCategories)
    showCategory(newCategories)
+}
+
+const editForm = (id) =>{
+   const editSelectedCategory = get('categories').find(category => category.id === id)
+   $('#nameCategoryEdit').value = editSelectedCategory.category
 }
 
 const initializer = () =>{
